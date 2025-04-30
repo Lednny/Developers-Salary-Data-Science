5112671a2ab75aa4e2f3c4d3f5ef53a933f41787
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-30 10:31:24 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from external.json_db_path import get_file_with_saved_path_content
+from clear_db import clear_db
 import os
 import pandas as pd
 import sys
 
 DB_PATH = get_file_with_saved_path_content()
 
-
 def main():
     if not DB_PATH or not os.path.exists(DB_PATH):
         print('Not exists DB, Please download with \'download_db.py\' script')
         sys.exit(-1)
 
-    DATA = pd.read_csv(DB_PATH)
+    CLEANED_DB_PATH = clear_db(DB_PATH)
+    if not CLEANED_DB_PATH: sys.exit(-1)
+
+    DATA = pd.read_csv(CLEANED_DB_PATH)
+    print("DataFrame:\n", DATA.head(10))
 
 
 if __name__ == "__main__":
