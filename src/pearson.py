7b3a136a0603fe11f40b14e7dbd0a28cc6ec48af
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-30 11:30:38 -0600

    Build beta pearson script file

--- src/pearson.py
+++ src/pearson.py
@@ -1,4 +1,14 @@
+from pandas import DataFrame, Series
 
-
-def main():
-    pass
+def main(df: DataFrame, columns: list[str] | None = None) -> int:
+    if columns is not None and len(columns) >= 2:
+        col1 = df[columns[0]]
+        col2 = df[columns[1]]
+        if type(col1) == Series and type(col2) == Series:
+            correlation = col1.corr(col2, method='pearson')
+            print(f"Correlación de Pearson entre '{columns[0]}' y '{columns[1]}': {correlation}")
+            return 0
+    else:
+        print("Correlación de Pearson entre todas las columnas:")
+        print(df.corr(method='pearson'))
+    return 0
