08288759478c1b7913278f7866bf37e425b45208
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-30 08:31:38 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+from external.json_db_path import get_file_with_saved_path_content
+import os
+import pandas as pd
+import sys
+
+DB_PATH = get_file_with_saved_path_content()
 
 
 def main():
-    pass
+    if not DB_PATH or not os.path.exists(DB_PATH):
+        print('Not exists DB, Please download with \'download_db.py\' script')
+        sys.exit(-1)
+
+    DATA = pd.read_csv(DB_PATH)
 
 
 if __name__ == "__main__":
