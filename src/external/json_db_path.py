77a5163b10b1d61648adef6731af2e6dfb891555
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-30 08:21:36 -0600

    Add protection if not exists file with db path

--- src/external/json_db_path.py
+++ src/external/json_db_path.py
@@ -4,13 +4,16 @@ from json import load, dump
 FILE_WITH_SAVED_PATH = "db_path.json"
 
 
-def get_file_with_saved_path_content(OBJ_TO_GET: str = 'PATH') -> str:
-    with open(FILE_WITH_SAVED_PATH, 'r') as f:
-        JSON_CONTENT = load(f)
-        try:
-            return JSON_CONTENT[OBJ_TO_GET]
-        except Exception:
-            return JSON_CONTENT
+def get_file_with_saved_path_content(OBJ_TO_GET: str = 'PATH') -> str | None:
+    try:
+        with open(FILE_WITH_SAVED_PATH, 'r') as f:
+            JSON_CONTENT = load(f)
+            try:
+                return JSON_CONTENT[OBJ_TO_GET]
+            except Exception:
+                return None
+    except Exception:
+        return None
 
 
 def saved_db_path_in_file(PATH) -> bool:
@@ -25,6 +28,7 @@ def saved_db_path_in_file(PATH) -> bool:
 def data_set_exists(_PRINT: bool = True) -> bool:
     if path.exists(FILE_WITH_SAVED_PATH):
         DB_PATH = get_file_with_saved_path_content()
+        if not DB_PATH: return False
         if path.exists(DB_PATH):
             if _PRINT: print(f'DataSet ALREADY EXISTS in {DB_PATH}')
             return True
