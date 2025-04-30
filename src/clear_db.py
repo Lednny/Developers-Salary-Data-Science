b557fdfef9e6750223d63523697aaad0107e25be
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-30 10:45:16 -0600

    Add update or restore feature to cleaned db

--- src/clear_db.py
+++ src/clear_db.py
@@ -5,7 +5,10 @@ CLEANED_DB_PATH = "cleaned_db.csv"
 
 def clear_db(DB_PATH: str) -> str | None: # Return the path of the cleaned database
     if os.path.exists(CLEANED_DB_PATH):
-        return CLEANED_DB_PATH
+        if input('CLEANDED DB ALREADY EXISTS, DO YOU WANT TO UPDATE or RESTORE [Y/n]? ').lower() != 'y':
+            return CLEANED_DB_PATH
+        else:
+            os.remove(CLEANED_DB_PATH)
 
     print("Cleaning DataSet...")
     df = pd.read_csv(DB_PATH)
