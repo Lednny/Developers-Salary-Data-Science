e867478880b399d9278ebb5316231a633b000c90
Ian2403 <AL03101404@tecmilenio.mx>
2025-05-01 13:28:39 -0600

    Update central_tendency.py

--- src/central_tendency.py
+++ src/central_tendency.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import os 
 
+media = datos.mean()
+mediana = datos.median()
+moda = datos.mode()[0]  
+
+print("Media:", media)
+print("Mediana:", mediana)
+print("Moda:", moda.tolist()) 
