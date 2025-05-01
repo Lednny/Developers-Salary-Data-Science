bbc358f8928d3afdc664e291a339cf555aedb1ac
Ian2403 <AL03101404@tecmilenio.mx>
2025-05-01 14:40:22 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,12 +22,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-def central_tendency():
-    media = serie.mean()
-    mediana = serie.median()
-    moda = serie.mode()
-  return {
-        "media": media,
-        "mediana": mediana,
-        "moda": moda,
-  }
