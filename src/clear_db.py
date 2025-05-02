025eced2b0546eb743dc87b5865f924000af01ea
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-05-02 00:51:43 -0600

    Add not-clear flag

--- src/clear_db.py
+++ src/clear_db.py
@@ -1,11 +1,13 @@
+from sys import argv as ARGS
 import pandas as pd
 import os
 
 CLEANED_DB_PATH = "cleaned_db.csv"
+FLAGS = { "clear": bool("--not-clear" in ARGS) }
 
 def clear_db(DB_PATH: str) -> str | None: # Return the path of the cleaned database
     if os.path.exists(CLEANED_DB_PATH):
-        if input('CLEANDED DB ALREADY EXISTS, DO YOU WANT TO UPDATE or RESTORE [Y/n]? ').lower() != 'y':
+        if FLAGS["clear"] or input('CLEANDED DB ALREADY EXISTS, DO YOU WANT TO UPDATE or RESTORE [Y/n]? ').lower() != 'y':
             return CLEANED_DB_PATH
         else:
             os.remove(CLEANED_DB_PATH)
@@ -16,5 +18,5 @@ def clear_db(DB_PATH: str) -> str | None: # Return the path of the cleaned datab
     df = df.dropna()
     df['Company Score'] = pd.to_numeric(df['Company Score'], errors='coerce')
     df.to_csv(CLEANED_DB_PATH, index=False)
-    print("Done!")
+    print("Done!\n")
     return CLEANED_DB_PATH
