c0cc91a70904f1b067b56bb7170d89c1a54a5a1c
Ian2403 <AL03101404@tecmilenio.mx>
2025-05-01 20:16:20 -0600

    Update central_tendency.py

--- src/central_tendency.py
+++ src/central_tendency.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
-def central_tendency(serie):
+def central_tendency(serie, imprimir=True):
     if not isinstance(serie, pd.Series):
         serie = pd.Series(serie)
-    return {
+    
+    result = {
         "media": serie.mean(),
         "mediana": serie.median(),
-        "moda": serie.mode().tolist()
+        "moda": serie.mode().tolist(), 
+        "varianza": serie.var(),
+        "desviacion_estandar": serie.std()
     }
+    
+    if imprimir:
+        print("Resultados de las tendcias centrales: ")
+        print(result)
+    
+    return result
+
